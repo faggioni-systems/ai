0f77aac6d8f64e2f3e16f222b2206a676148ca8b
Miguel Faggioni <miguel@faggioni.dev>
2025-09-19 00:57:35 -0400

    model

--- app/OpenAI/OpenAIDataHelper.php
+++ app/OpenAI/OpenAIDataHelper.php
@@ -13,12 +13,19 @@ class OpenAIDataHelper
                     'type' => 'web_search_preview'
                 ]
             ],
-            'input' => $input,
+            'input' => $this->getResponseInstruction($input),
             'temperature' => 0.7,
             'max_output_tokens' => 10000,
             'tool_choice' => 'auto',
             'parallel_tool_calls' => true,
             'store' => true,
         ];
+    }
+    private function getResponseInstruction(string $input): string
+    {
+        return "You are a helpful project manager. Use the following pieces of
+                information to give a broader context on how to fix issues from the developer stand point.
+                You don't need to give the answer, you just need to re-write the context on a readable way. Context: "
+                . $input;
     }
 }
